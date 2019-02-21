88d0f96424685d1fd34ab40a10c753ea4fa5acb9
PratikshaNaithani <41237831+PratikshaNaithani@users.noreply.github.com>
2019-02-21 18:22:38 +0530

    Add files via upload

---
+++ Login/MvcLoginRegistration/MvcLoginRegistration/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcLoginRegistration.Models;
+
+namespace MvcLoginRegistration.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if(ModelState.IsValid)
+            {
+                using (OurDbContext db = new OurDbContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + " " + account.LastName + "successfully registered.";
+            }
+            return View();
+        }
+        //Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserAccount account)
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                var usr = db.userAccount.Single(u => u.Username == user.Username && u.Password == user.Password);
+                if(usr!= null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ Login/MvcLoginRegistration/MvcLoginRegistration/Models/UserAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLoginRegistration.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="FirstName is required")]
+        public String FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        public String LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$", ErrorMessage ="please enter valid email")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public String Username { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Please confirm your password")]
+        [DataType(DataType.Password)]
+        public String ConfirmPassword { get; set; }
+    }
+}
